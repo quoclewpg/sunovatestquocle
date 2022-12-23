82855d7aea4e46f3a59da9de47870ce139cf2fc2
= <=>
2022-12-23 14:21:11 -0600

    json file

--- js/server.js
+++ js/server.js
@@ -1,12 +1,14 @@
-window.onload = function loadServers(){
+/*window.onload = function loadServers(){
 	var xHttp = new XMLHttpRequest();
-	xHttp.open('GET', 'https://mobiledev.sunovacu.ca/api/Test/GetCars', true);
+	xHttp.open('POST', 'https://apps.mpi.mb.ca/DriverQuiz/api/DrivingQuizzes.svc/StartQuiz', true);
 
 	xHttp.onload = function(){
 		if(this.status == 200)
 		{
-			var cars = JSON.parse(this.responseText);
+			var quiz = JSON.parse(this.responseText);
 			var table = "";
+
+			console.log(quiz);
 			
 			for(var i = 0; i < cars.length; i++)
 			{
@@ -16,11 +18,11 @@ window.onload = function loadServers(){
 					  + cars[i].engine + "</td><td>"
 					  + cars[i].color + "</td></tr>";
 				document.getElementById("sortable").innerHTML = table;		
-			}	
+			}
 		}
 	}
 	xHttp.send();
-}
+}*/
 
 function search(){
 	var inputSearch, data, filterSearch, table, th, tr, td;
@@ -52,4 +54,22 @@ function search(){
 		}
 		data = "";
  	}
+}
+
+window.onload = function getQuiz() {
+	var urlGet = '../DriverQuiz/api/DrivingQuizzes.svc/StartQuiz';
+	var inData = { "classId": 5, "classCategory": 0, "questionQty": 20 };
+	var json = null;
+	$.ajax({
+		url: urlGet,
+		type: "POST",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		data: JSON.stringify(inData),
+		async: false,
+		success: function (data) {
+			json = convertLangDictionaryArray(data, 'translations');
+		}
+	});
+	return json;
 }
